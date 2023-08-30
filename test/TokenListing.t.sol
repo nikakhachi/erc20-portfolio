73e12f10e0e62ef2bdc67cf7fecc57c89af44a36
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-08-30 18:57:40 +0400

    add tests in TokenListing

--- test/TokenListing.t.sol
+++ test/TokenListing.t.sol
@@ -3,19 +3,39 @@ pragma solidity ^0.8.20;
 
 import "./ERC20Portfolio.t.sol";
 
+/**
+ * @title TokenListingTest Contract
+ * @author Nika Khachiashvili
+ * @dev Contract for testing token listing functionality
+ */
 contract TokenListingTest is ERC20PortfolioTest {
-    /// @dev testing the listing of token
-    function testListTokenFuzz(address _token) public {
+    /// @dev testing the listing of token on empty portfolio
+    function testListTokenOnEmptyPortfolioFuzz(address _token) public {
         portfolio.listToken(_token);
         assertEq(portfolio.getSupportedTokensCount(), 1);
         assertEq(portfolio.supportedTokens(0), _token);
         assertEq(portfolio.getSupportedTokenId(_token), 1);
     }
 
+    /// @dev testing the listing of token on empty portfolio
+    function testListTokenOnExistingPortfolioFuzz(uint8 amount) public {
+        vm.assume(amount < 20); /// @dev Just to make tests faster
+
+        for (uint8 i = 0; i < amount; i++) {
+            portfolio.listToken(address(new ERC20("Test", "TEST")));
+        }
+
+        portfolio.listToken(address(token));
+
+        assertEq(portfolio.getSupportedTokensCount(), amount + 1);
+        assertEq(portfolio.supportedTokens(amount), address(token));
+        assertEq(portfolio.getSupportedTokenId(address(token)), amount + 1);
+    }
+
     /// @dev testing the listing of token
     function testListExistingTokenFuzz(address _token) public {
         portfolio.listToken(_token);
-        vm.expectRevert();
+        vm.expectRevert(ERC20Portfolio.InvalidToken.selector);
         portfolio.listToken(_token);
     }
 }
